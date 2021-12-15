23c918e91aa4dcb2bcfb6400def4af0fbcaf5d93
Jason Lau <lau@cs.ucla.edu>
2021-12-14 19:51:37 -0800

    test(mlir-clang): fix ubuntu 20.04 gettimeofday

--- tools/mlir-clang/Test/Verification/gettimeofday.c
+++ tools/mlir-clang/Test/Verification/gettimeofday.c
@@ -9,26 +9,26 @@ double alloc() {
 }
 
 // CHECK:   func @alloc() -> f64
-// CHECK-NEXT:     %cst = arith.constant 9.9999999999999995E-7 : f64
-// CHECK-NEXT:     %c1_i32 = arith.constant 1 : i32
-// CHECK-NEXT:     %c0_i32 = arith.constant 0 : i32
-// CHECK-NEXT:     %c1_i64 = arith.constant 1 : i64
-// CHECK-NEXT:     %0 = llvm.alloca %c1_i64 x !llvm.struct<(i64, i64)> : (i64) -> !llvm.ptr<struct<(i64, i64)>>
-// CHECK-NEXT:     %1 = memref.alloca() : memref<1x2xi64>
-// CHECK-NEXT:     %2 = llvm.mlir.null : !llvm.ptr<struct<(i32, i32)>>
-// CHECK-NEXT:     %3 = llvm.call @gettimeofday(%0, %2) : (!llvm.ptr<struct<(i64, i64)>>, !llvm.ptr<struct<(i32, i32)>>) -> i32
-// CHECK-NEXT:     %4 = llvm.getelementptr %0[%c0_i32, %c0_i32] : (!llvm.ptr<struct<(i64, i64)>>, i32, i32) -> !llvm.ptr<i64>
-// CHECK-NEXT:     %5 = llvm.load %4 : !llvm.ptr<i64>
-// CHECK-NEXT:     affine.store %5, %1[0, 0] : memref<1x2xi64>
-// CHECK-NEXT:     %6 = llvm.getelementptr %0[%c0_i32, %c1_i32] : (!llvm.ptr<struct<(i64, i64)>>, i32, i32) -> !llvm.ptr<i64>
-// CHECK-NEXT:     %7 = llvm.load %6 : !llvm.ptr<i64>
-// CHECK-NEXT:     affine.store %7, %1[0, 1] : memref<1x2xi64>
-// CHECK-NEXT:     %8 = memref.subview %1[0, 0] [1, 2] [1, 1] : memref<1x2xi64> to memref<2xi64>
-// CHECK-NEXT:     %9 = affine.load %8[0] : memref<2xi64>
-// CHECK-NEXT:     %10 = arith.sitofp %9 : i64 to f64
-// CHECK-NEXT:     %11 = affine.load %8[1] : memref<2xi64>
-// CHECK-NEXT:     %12 = arith.sitofp %11 : i64 to f64
-// CHECK-NEXT:     %13 = arith.mulf %12, %cst : f64
-// CHECK-NEXT:     %14 = arith.addf %10, %13 : f64
-// CHECK-NEXT:     return %14 : f64
-// CHECK-NEXT:   }
+// CHECK:     %cst = arith.constant 9.9999999999999995E-7 : f64
+// CHECK:     %c1_i32 = arith.constant 1 : i32
+// CHECK:     %c0_i32 = arith.constant 0 : i32
+// CHECK:     %c1_i64 = arith.constant 1 : i64
+// CHECK:     %[[ALLOC:.*]] = llvm.alloca %c1_i64 x !llvm.struct<(i64, i64)> : (i64) -> !llvm.ptr<struct<(i64, i64)>>
+// CHECK:     %[[VAL:.*]] = memref.alloca() : memref<1x2xi64>
+// CHECK:     %[[NULL:.*]] = llvm.mlir.null : !llvm.ptr<struct<(i32, i32)>>
+// CHECK:     %{{.*}} = llvm.call @gettimeofday(%[[ALLOC]], %[[NULL]]) : (!llvm.ptr<struct<(i64, i64)>>, !llvm.ptr<struct<(i32, i32)>>) -> i32
+// CHECK:     %[[D4:.*]] = llvm.getelementptr %[[ALLOC]][%c0_i32, %c0_i32] : (!llvm.ptr<struct<(i64, i64)>>, i32, i32) -> !llvm.ptr<i64>
+// CHECK:     %[[D5:.*]] = llvm.load %[[D4]] : !llvm.ptr<i64>
+// CHECK:     affine.store %[[D5]], %[[VAL]][0, 0] : memref<1x2xi64>
+// CHECK:     %[[D6:.*]] = llvm.getelementptr %[[ALLOC]][%c0_i32, %c1_i32] : (!llvm.ptr<struct<(i64, i64)>>, i32, i32) -> !llvm.ptr<i64>
+// CHECK:     %[[D7:.*]] = llvm.load %[[D6]] : !llvm.ptr<i64>
+// CHECK:     affine.store %[[D7]], %[[VAL]][0, 1] : memref<1x2xi64>
+// CHECK:     %[[SUBVIEW:.*]] = memref.subview %[[VAL]][0, 0] [1, 2] [1, 1] : memref<1x2xi64> to memref<2xi64>
+// CHECK:     %[[SUBVIEW0:.*]] = affine.load %[[SUBVIEW]][0] : memref<2xi64>
+// CHECK:     %[[SUBVIEW0F:.*]] = arith.sitofp %[[SUBVIEW0]] : i64 to f64
+// CHECK:     %[[SUBVIEW1:.*]] = affine.load %[[SUBVIEW]][1] : memref<2xi64>
+// CHECK:     %[[SUBVIEW1F:.*]] = arith.sitofp %[[SUBVIEW1]] : i64 to f64
+// CHECK:     %[[D13:.*]] = arith.mulf %[[SUBVIEW1F]], %cst : f64
+// CHECK:     %[[RET:.*]] = arith.addf %[[SUBVIEW0F]], %[[D13]] : f64
+// CHECK:     return %[[RET]] : f64
+// CHECK:   }
